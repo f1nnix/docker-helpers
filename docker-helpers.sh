9e0337e02ed2b010084775f8ca117b3f492c1266
Ilya Rusanen <ilya@rusanen.co.uk>
2017-01-02 01:48:22 +0300

    [feat] +list functions

---
+++ docker-helpers.sh
@@ -0,0 +1,90 @@
+function print_header() {
+  TEXT=$1
+  TEXT_LENGTH=${#TEXT} 
+  TERMINAL_COLS=$(tput cols)
+  SECTION_LENGTH=$(((TERMINAL_COLS-TEXT_LENGTH)/2))
+  SECTION=$(printf "%-${SECTION_LENGTH}s" "=")
+  # echo "$SECTION$TEXT$SECTION"
+  echo "${SECTION// /=}$TEXT${SECTION// /=}"
+}
+
+function docker_machine_list() {
+  docker-machine ls
+}
+
+function docker_machine_ssh() {
+  MACHINE=""
+  if [ -z "$1" ]
+  then
+    echo "Nothing to ssh. Exiting"
+    return 0
+  else
+    MACHINE="$1"
+
+  fi
+
+  docker-machine ssh $MACHINE
+}
+
+function docker_machine_regenerate_certs() {
+  MACHINE=""
+  if [ -z "$1" ]
+  then
+    echo "Nothing to regenerate. Exiting"
+    return 0
+  else
+    MACHINE="$1"
+
+  fi
+
+  docker-machine regenerate-certs $MACHINE
+}
+
+function docker_machine_env() {
+  MACHINE=""
+  if [ -z "$1" ]
+  then
+    echo "Nothing to start. Exiting"
+    return 0
+  else
+    MACHINE="$1"
+
+  fi
+
+  docker-machine start $MACHINE
+  docker-machine env $MACHINE
+  eval $(docker-machine env $MACHINE)
+  # stats
+  print_header "VOLUMES"
+  docker volume ls
+  print_header "IMAGES"
+  docker images
+  print_header "CONTAINERS"
+  docker ps -a
+}
+
+function docker_constainer_list() {
+  docker ps -a
+}
+
+function docker_constainer_bash() {
+  CONTAINER=""
+  if [ -z "$1" ]
+  then
+    echo "no container specifies. Exiting"
+    return 0
+  else
+    CONTAINER="$1"
+  fi
+  docker exec -it $CONTAINER bash
+
+}
+
+# docker-machine shortcuts
+alias dml="docker_machine_list"
+alias dms="docker_machine_ssh $1"
+alias dme="docker_machine_env $1"
+alias dmrc="docker_machine_regenerate_certs $1"
+# docker shortcuts
+alias dcl="docker_constainer_list"
+alias dcb="docker_constainer_bash $1"
